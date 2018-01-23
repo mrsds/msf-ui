9e214724d171ece2651a16b62c3277743ccf4739
aplave <aaron.plave@jpl.nasa.gov>
2018-01-22 22:18:32 -0800

    Logo tweak.

--- src/components/AppBar/AppLogo.js
+++ src/components/AppBar/AppLogo.js
@@ -2,63 +2,72 @@ import React from "react";
 import SvgIcon from "material-ui/SvgIcon";
 
 let AppLogo = () => (
-    <svg width="32" height="31" viewBox="0 0 32 31" version="1.1">
-        <g id="Canvas" transform="translate(9900 -5484)">
+    <svg viewBox="0 0 30 30" version="1.1">
+        <g id="Canvas" transform="translate(-10 -5509)">
             <g id="Group 2.4">
                 <mask id="mask0_alpha" mask-type="alpha">
                     <g id="Rectangle 112.3">
                         <use
                             xlinkHref="#path0_fill"
-                            transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9890.89 5480.64)"
+                            transform="matrix(2.49898e-16 1 1 -2.82059e-16 10 5507)"
+                            fill="#FFFFFF"
                         />
                     </g>
                 </mask>
                 <g id="Rectangle 112.11" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path1_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9871.66 5497.43)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 36.2505 5518.07)"
+                        fill="#FFFFFF"
                     />
                 </g>
                 <g id="Rectangle 112.4" opacity="0.875" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path2_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9875.69 5498.9)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 32.5014 5521.03)"
+                        fill="#FFFFFF"
                     />
                 </g>
                 <g id="Rectangle 112.5" opacity="0.75" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path3_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9876.59 5490.93)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 28.7517 5512.9)"
+                        fill="#FFFFFF"
                     />
                 </g>
                 <g id="Rectangle 112.6" opacity="0.625" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path4_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9879.43 5488.79)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 25.001 5511.63)"
+                        fill="#FFFFFF"
                     />
                 </g>
                 <g id="Rectangle 112.7" opacity="0.5" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path5_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9884.04 5492.05)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 21.2512 5516.69)"
+                        fill="#FFFFFF"
                     />
                 </g>
                 <g id="Rectangle 112.8" opacity="0.375" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path6_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9885.21 5484.87)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 17.5013 5509.5)"
+                        fill="#FFFFFF"
                     />
                 </g>
                 <g id="Rectangle 112.9" opacity="0.25" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path7_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9889.25 5486.39)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 13.7518 5512.52)"
+                        fill="#FFFFFF"
                     />
                 </g>
                 <g id="Rectangle 112.10" opacity="0.125" mask="url(#mask0_alpha)">
                     <use
                         xlinkHref="#path8_fill"
-                        transform="matrix(-0.313455 0.949603 0.949603 0.313455 -9895.08 5493.33)"
+                        transform="matrix(2.49898e-16 1 1 -2.82059e-16 10.0015 5521.91)"
+                        fill="#FFFFFF"
                     />
                 </g>
             </g>
@@ -66,16 +75,16 @@ let AppLogo = () => (
         <defs>
             <path
                 id="path0_fill"
-                d="M 0 13.4432C 0 6.01873 6.01873 0 13.4432 0L 13.4432 0C 20.8677 0 26.8864 6.01873 26.8864 13.4432L 26.8864 13.4432C 26.8864 20.8677 20.8677 26.8864 13.4432 26.8864L 13.4432 26.8864C 6.01873 26.8864 0 20.8677 0 13.4432L 0 13.4432Z"
+                d="M 0 15C 0 6.71573 6.71573 0 15 0L 15 0C 23.2843 0 30 6.71573 30 15L 30 15C 30 23.2843 23.2843 30 15 30L 15 30C 6.71573 30 0 23.2843 0 15L 0 15Z"
             />
-            <path id="path1_fill" d="M 0 0L 16.9657 0L 16.9657 3.3608L 0 3.3608L 0 0Z" />
-            <path id="path2_fill" d="M 0 0L 14.3126 0L 14.3126 3.3608L 0 3.3608L 0 0Z" />
-            <path id="path3_fill" d="M 0 0L 21.5958 0L 21.5958 3.3608L 0 3.3608L 0 0Z" />
-            <path id="path4_fill" d="M 0 0L 22.7315 0L 22.7315 3.3608L 0 3.3608L 0 0Z" />
-            <path id="path5_fill" d="M 0 0L 18.1966 0L 18.1966 3.3608L 0 3.3608L 0 0Z" />
-            <path id="path6_fill" d="M 0 0L 24.6456 0L 24.6456 3.3608L 0 3.3608L 0 0Z" />
-            <path id="path7_fill" d="M 0 0L 21.9376 0L 21.9376 3.3608L 0 3.3608L 0 0Z" />
-            <path id="path8_fill" d="M 0 0L 13.5228 0L 13.5228 3.3608L 0 3.3608L 0 0Z" />
+            <path id="path1_fill" d="M 0 0L 18.9304 0L 18.9304 3.75L 0 3.75L 0 0Z" />
+            <path id="path2_fill" d="M 0 0L 15.9701 0L 15.9701 3.75L 0 3.75L 0 0Z" />
+            <path id="path3_fill" d="M 0 0L 24.0967 0L 24.0967 3.75L 0 3.75L 0 0Z" />
+            <path id="path4_fill" d="M 0 0L 25.3639 0L 25.3639 3.75L 0 3.75L 0 0Z" />
+            <path id="path5_fill" d="M 0 0L 20.3038 0L 20.3038 3.75L 0 3.75L 0 0Z" />
+            <path id="path6_fill" d="M 0 0L 27.4997 0L 27.4997 3.75L 0 3.75L 0 0Z" />
+            <path id="path7_fill" d="M 0 0L 24.4781 0L 24.4781 3.75L 0 3.75L 0 0Z" />
+            <path id="path8_fill" d="M 0 0L 15.0888 0L 15.0888 3.75L 0 3.75L 0 0Z" />
         </defs>
     </svg>
 );
