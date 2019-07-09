291c24236c3886cb41c546f241ab39257bddc408
rtapella <rtapella@jpl.nasa.gov>
2019-07-08 17:36:30 -0700

    moved around again!

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -40,31 +40,33 @@ export class LandingPage extends Component {
                 <LoadingContainerExtended />
                 <div className={styles.container}>
                     <div className={styles.hero_banner}>
-                        <div className={styles.banner_shader} />>
-                        <div className={styles.brand_area}>
-                            <div className={styles.brand1}>
-                                <a href="http://www.nasa.gov" className={styles.nasa_logo} />
-                            </div>
-                            <div className={styles.brand2}>
-                                <div className={styles.jpl_logo}>
-                                    <a href="http://jpl.nasa.gov" className={styles.nasa_logo}>
-                                        Jet Propulsion Laboratory
-                                    </a>
-                                </div>
-                                <div className={styles.caltech_logo}>
-                                    <a href="http://caltech.edu" className={styles.nasa_logo}>
-                                        California Institute of Technology
-                                    </a>
-                                </div>
-                            </div>
-                            <img
-                                alt="NASA Logo"
-                                className={styles.print_logo}
-                                src="img/logo_nasa_trio_white.png"
-                            />
-                        </div>
+                        <div className={styles.banner_shader} />
                         
                         <div className={styles.hero_controls}>
+
+                            <div className={styles.brand_area}>
+                                <div className={styles.brand1}>
+                                    <a href="http://www.nasa.gov" className={styles.nasa_logo} />
+                                </div>
+                                <div className={styles.brand2}>
+                                    <div className={styles.jpl_logo}>
+                                        <a href="http://jpl.nasa.gov" className={styles.nasa_logo}>
+                                            Jet Propulsion Laboratory
+                                        </a>
+                                    </div>
+                                    <div className={styles.caltech_logo}>
+                                        <a href="http://caltech.edu" className={styles.nasa_logo}>
+                                            California Institute of Technology
+                                        </a>
+                                    </div>
+                                </div>
+                                <img
+                                    alt="NASA Logo"
+                                    className={styles.print_logo}
+                                    src="img/logo_nasa_trio_white.png"
+                                />
+                            </div>
+
                             <Typography className={styles.title} color="textSecondary" gutterBottom>
                                 <br></br>Methane Source Finder
                             </Typography>
