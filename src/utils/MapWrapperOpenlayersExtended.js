733004bdb024475cb76e84bc813ff0adb5052695
Joshua D Rodriguez <jdrodrig@jpl.nasa.gov>
2019-09-19 15:24:25 -0700

    Show/hide map labels does not do anything #346

--- src/utils/MapWrapperOpenlayersExtended.js
+++ src/utils/MapWrapperOpenlayersExtended.js
@@ -310,7 +310,10 @@ export default class MapWrapperOpenlayersExtended extends MapWrapperOpenlayers {
         try {
             const mapLayers = this.map.getLayers();
             let insertIndex = 0;
-            if (mapLayer.get("_layerType") === appStrings.LAYER_GROUP_TYPE_BASEMAP) {
+            if (
+                mapLayer.get("_layerType") === appStrings.LAYER_GROUP_TYPE_BASEMAP ||
+                appStrings.LAYER_GROUP_TYPE_REFERENCE
+            ) {
                 insertIndex = this.findTopInsertIndexForLayer(mapLayer);
             }
 
@@ -1192,5 +1195,40 @@ export default class MapWrapperOpenlayersExtended extends MapWrapperOpenlayers {
             .getLayers()
             .getArray()
             .filter(l => l.get("_layerGroup") === "GRIDDED");
+    }
+
+    /**
+     * Find the highest index for a layer to be displayed.
+     * Data layers are displayed below reference layers and
+     * above basemaps
+     *
+     * @param {object} mapLayer openlayers map layer to compare
+     * @returns {number} highest index display index for a layer of this type
+     * @memberof MapWrapperOpenlayers
+     */
+    findTopInsertIndexForLayer(mapLayer) {
+        let mapLayers = this.map.getLayers();
+        let index = mapLayers.getLength();
+
+        if (mapLayer.get("_layerType") === appStrings.LAYER_GROUP_TYPE_REFERENCE) {
+            // referece layers always on top of basemaps
+            return 1;
+        } else if (mapLayer.get("_layerType") === appStrings.LAYER_GROUP_TYPE_BASEMAP) {
+            // basemaps always on bottom
+            return 0;
+        } else {
+            // data layers in the middle
+            for (let i = index - 1; i >= 1; --i) {
+                let compareLayer = mapLayers.item(i);
+                if (
+                    compareLayer.get("_layerType") === appStrings.LAYER_GROUP_TYPE_DATA ||
+                    compareLayer.get("_layerType") === appStrings.LAYER_GROUP_TYPE_BASEMAP
+                ) {
+                    return i + 1;
+                }
+            }
+            index = 0;
+        }
+        return index;
     }
 }
