4558f9efb305da15550ae047f2ea4e8ede07949e
Steve Zelinka <szelinka@arb.ca.gov>
2020-09-12 17:52:05 -0700

    Fixed contrast issue when dropdown menu is active

--- src/components/Reusables/ChipDropdown.js
+++ src/components/Reusables/ChipDropdown.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import MiscUtil from "_core/utils/MiscUtil";
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import Chip from "@material-ui/core/Chip";
-import styles from "components/Reusables/ChipDropdownStyles.scss";
+//import styles from "components/Reusables/ChipDropdownStyles.scss";
+import styles from "components/Reusables/ChipDropdownStylesHiContrast.scss"; // Can this just append ChipDropdownStyles.scss where needed? duplication of remainder
 
 const ChipDropdown = props => {
     let rootClasses = MiscUtil.generateStringFromSet({
