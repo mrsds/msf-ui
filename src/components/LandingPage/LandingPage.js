76bc5e1ed95ff067520c44e309eff0c5268dbfba
Rob Tapella <rtapella@jpl.nasa.gov>
2023-03-13 10:11:20 -0700

    small style updates to landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -157,9 +157,9 @@ export class LandingPage extends Component {
                                     approach to decrease overall atmospheric radiative forcing. Methane and carbon
                                     dioxide have distinct absorption features in the shortwave infrared (1900&ndash;2500 nm)
                                     that permits mapping of point source greenhouse gas emissions with imaging
-                                    spectrometers that were developed at JPL, like <a href="https://aviris.jpl.nasa.gov/">AVIRIS</a>,
-                                    <a href="https://avirisng.jpl.nasa.gov/greenhouse_gas_mapping.html">AVIRIS-NG</a>,
-                                    <a href="https://doi.org/10.1109/AERO53065.2022.9843565">AVIRIS-3</a>,
+                                    spectrometers that were developed at JPL, like <a href="https://aviris.jpl.nasa.gov/">AVIRIS</a>, 
+                                    <a href="https://avirisng.jpl.nasa.gov/greenhouse_gas_mapping.html">AVIRIS-NG</a>, 
+                                    <a href="https://doi.org/10.1109/AERO53065.2022.9843565">AVIRIS-3</a>, 
                                     and <a href="https://earth.jpl.nasa.gov/emit/">EMIT</a>. 
                                     Future NASA missions like the Surface Biology and Geology
                                     (<a href="https://sbg.jpl.nasa.gov/">SBG</a>)
