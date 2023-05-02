067b2f7cba43f6f6578e16f8863b3587cef71745
Joshua Rodriguez (i.e. Freddie Gibbs) <jdrodrig@jpl.nasa.gov>
2023-05-02 11:48:53 -0700

    Update DataFilterContainer.js

--- src/components/MSFAnalytics/DataFilterContainer.js
+++ src/components/MSFAnalytics/DataFilterContainer.js
@@ -213,9 +213,9 @@ export class DataFilterContainer extends Component {
         const downloadSettings = (_ => {
             switch (this.props.analyticsMode) {
                 case MSFTypes.ANALYTICS_MODE_PLUME_DETECTION_STATS:
-                    return ["Download original plume lists", appConfig.URLS.plumeListDownload];
+                    return ["Download original plume list", appConfig.URLS.plumeListDownload];
                 case MSFTypes.ANALYTICS_MODE_EMISSIONS_SUMMARY_INFO:
-                    return ["Download original source lists", appConfig.URLS.sourceListDownload];
+                    return ["Download original source list", appConfig.URLS.sourceListDownload];
                 default:
                     null;
             }
